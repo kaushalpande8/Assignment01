61ea710d42aeced810ce712409d569d89ac2df1e
Kaushal <Kaushalpande8@gmail.com>
2020-02-06 22:09:16 -0500

    Containing Rectangle Class

---
+++ Assignment01/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace Assignment01
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Rectangle r = new Rectangle();
+            int userChoose;
+            do
+            {
+
+                Console.WriteLine("Please Enter Valid Input From Below Option:");
+                Console.WriteLine("1.Get Rectangle Lenght");
+                Console.WriteLine("2.Change Rectangle Lenght");
+                Console.WriteLine("3.Get Rectangle Width");
+                Console.WriteLine("4.Change Rectangle Width");
+                Console.WriteLine("5.Get Rectangle Perimeter");
+                Console.WriteLine("6.Get Rectangle Area");
+                Console.WriteLine("7.Exit");
+                userChoose = int.Parse(Console.ReadLine());
+                
+
+            } while (true);
+         }
+    }
+}
---
+++ Assignment01/Rectangle.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Assignment01
+{
+     public class Rectangle
+    {
+        private int length;
+        private int width;
+
+        public int Lenght
+        {
+        get{ return length;}
+        set{length = value;}
+        }
+        public int Width
+        {
+            get { return width; }
+            set { width = value; }
+        }
+
+        public Rectangle()
+        {
+            Width = 1;
+            Lenght = 1;
+        }
+        public Rectangle(int Lenght, int Width)
+        {
+            this.Lenght = length;
+            this.Width = width;
+        }
+        public int GetLenght()
+        {
+            return Lenght;
+        }
+        public int SetLenght(int setLength)
+        {
+            this.Lenght = setLength;
+            return this.Lenght;
+
+        }
+        public int GetWidth()
+        {
+            return this.Width;
+
+        }
+        public int SetWidth(int width)
+        {
+            return this.Width = width;
+        }
+
+        public int GetPerimeter()
+        {
+            int perimeter = 2 * (this.Lenght + this.Width);
+            return perimeter;
+        }
+        public int GetArea()
+        {
+            int area = this.Lenght * this.Width;
+            return area;
+        }
+    }
+}
+
